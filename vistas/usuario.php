8062834f8e1b65ffc8c7f560a6b14b4e0942102a
Xeppyz <allan.silva13589@est.uca.edu.ni>
2022-11-23 23:16:13 -0600

    Vista perfil

--- vistas/usuario.php
+++ vistas/usuario.php
@@ -229,13 +229,13 @@ $cu = new usuarioController();
                     <li class="dropdown-header">
                         <h6>Kevin Anderson</h6>
                         <span>Web Designer</span>
-                    </li>
+
                     <li>
                         <hr class="dropdown-divider">
                     </li>
 
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                        <a class="dropdown-item d-flex align-items-center" href="users-profile.php">
                             <i class="bi bi-person"></i>
                             <span>My Profile</span>
                         </a>
