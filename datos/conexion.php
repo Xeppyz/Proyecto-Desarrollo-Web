67b793038384cb034f9d50e7a7892a8d01b1f1b4
Xeppyz <allan.silva13589@est.uca.edu.ni>
2022-11-24 01:42:55 -0600

    DEJEN LA CLASE CONEXION

---
+++ datos/conexion.php
@@ -0,0 +1,32 @@
+<?php
+
+class Conexion
+{
+    private string $host;
+    private string $db;
+    private string $user;
+    private string $password;
+    private string $charset;
+
+    public function __construct()
+    {
+        $this->host = "localhost";
+        $this->db = "dbkermesse";
+        $this->user = "root";
+        $this->password = "1234";
+        $this->charset = "utf8mb4";
+    }
+
+    public function conectar()
+    {
+        $con = "mysql:host={$this->host}; dbname={$this->db}; charset={$this->charset}";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+
+        $pdo = new PDO($con, $this->user, $this->password, $options);
+        return $pdo;
+    }
+}
