dde0a19b8d8cefcf28f5706972aa911221ec1e7e
josar <josarroliga0943@outlook.es>
2022-11-24 00:04:05 -0600

    loco odio mi vida

--- vistas/agregar_comunidad.php
+++ vistas/agregar_comunidad.php
@@ -1,11 +1,23 @@
 <?php
+
 require_once '../entidades/tbl_comunidad.php';
 require_once '../datos/dt_tbl_comunidad.php';
 require_once '../controladores/comunidadController.php';
 
-$tc = new tbl_comunidad();
-$dtc = new dt_tbl_comunidad();
-$cc = new comunidadController();
+if(isset($_POST['m'])){
+    $metodo = $_POST['m'];
+    if(method_exists("comunidadController", $metodo));
+}else{
+
+}
+
+if(isset($_POST['m'])){
+    $metodo = $_POST['m'];
+    if(method_exists("comunidadController",$metodo))
+    {
+        comunidadController::{$metodo}();
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +27,7 @@ $cc = new comunidadController();
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Kermesse - Lista de Comunidades</title>
+    <title>Components / Accordion - NiceAdmin Bootstrap Template</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -25,8 +37,7 @@ $cc = new comunidadController();
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -235,27 +246,27 @@ $cc = new comunidadController();
                     </li>
 
                     <li>
-                        <!--<a class="dropdown-item d-flex align-items-center" href="users-profile.html">-->
+                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                             <i class="bi bi-person"></i>
                             <span>My Profile</span>
-                        <!--</a>-->
+                        </a>
                     </li>
                     <li>
                         <hr class="dropdown-divider">
                     </li>
 
                     <li>
-                       <!-- <a class="dropdown-item d-flex align-items-center" href="users-profile.html">-->
+                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                             <i class="bi bi-gear"></i>
                             <span>Account Settings</span>
-                        <!--</a>-->
+                        </a>
                     </li>
                     <li>
                         <hr class="dropdown-divider">
                     </li>
 
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="">
+                        <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
                             <i class="bi bi-question-circle"></i>
                             <span>Need Help?</span>
                         </a>
@@ -288,12 +299,12 @@ include("shared/navbar.php");
 <main id="main" class="main">
 
     <div class="pagetitle">
-        <h1>Comunidades</h1>
+        <h1>Agregar Usuario</h1>
         <nav>
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Seguridad</a></li>
-                <li class="breadcrumb-item">Comunidades</li>
-
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item">Seguridad</li>
+                <li class="breadcrumb-item active">Agregar Usuario</li>
             </ol>
         </nav>
     </div><!-- End Page Title -->
@@ -301,49 +312,47 @@ include("shared/navbar.php");
     <section class="section">
         <div class="row">
             <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Comunidad</h5>
-                        <table class="table comunidadTable">
-                            <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nombre</th>
-                                <th>Responsable</th>
-                                <th>Descripcion</th>
-                                <th>Estado</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                            foreach ($dtc->listarComunidad() as $r):
-                                ?>
-                                <tr>
-                                    <td><?php echo $r->getIdComunidad(); ?></td>
-                                    <td><?php echo $r->getNombre(); ?></td>
-                                    <td><?php echo $r->getResponsable(); ?></td>
-                                    <td><?php echo $r->getDescContribucion(); ?></td>
-                                    <td><?php echo $r->getEstado(); ?></td>
-                                    <td>
-                                        <!--<a href="editar_comunidad.php?id_comunidad=<?php /*echo $r->getIdComunidad(); */?>">
-                                            <i class="bi bi-pencil-square" title="Editar comunidad"></i>
-                                        </a>-->
-                                        &nbsp;&nbsp;
-                                        <!--<a href="eliminar_comunidad.php?id_comunidad=<?php /*echo $r->getIdComunidad(); */?>">
-                                            <i class="bi bi-trash3" title="Eliminar comunidad"></i>
-                                        </a>-->
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                            </tbody>
-                        </table>
+                <form action="" method="POST">
+                    <div class="row mb-3">
+                        <input type="hidden" value="guardar" name="txtaccion" />
+                        <label class="col-sm-2 col-form-table">Nombre:</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="nombre" />
+                        </div>
                     </div>
-                </div>
-                <a href="agregar_comunidad.php">
-                    <button type="button" class="btn btn-outline-primary">Agregar comunidad</button>
-                </a>
-            </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-table">Apellido:</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="apellido" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-table">Email:</label>
+                        <div class="col-sm-10">
+                            <input type="email" name="email" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-table">Usuario:</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="usuario" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-table">Contraseña:</label>
+                        <div class="col-sm-10">
+                            <input type="password" name="pwd" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
 
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Guardar Usuario</button>
+                            <input type="hidden" name="m" value="guardarUsuario">
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
 
@@ -355,8 +364,7 @@ include("shared/footer.php");
 ?>
 <!-- End Footer -->
 
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-        class="bi bi-arrow-up-short"></i></a>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
 <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
@@ -370,15 +378,6 @@ include("shared/footer.php");
 
 <!-- Template Main JS File -->
 <script src="assets/js/main.js"></script>
-<script>
-    import {DataTable} from "./assets/vendor/simple-datatables/simple-datatables";
-
-    let usuarioTable = document.querySelector('.comunidadTable');
-    let dataTable = new DataTable(".comunidadTable", {
-        searchable: true,
-        fixedHeight: true
-    });
-</script>
 
 </body>
 
