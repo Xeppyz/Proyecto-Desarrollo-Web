e067fb5b7111324801e2d0ff9f29461b079f0ef7
mmajewsky133 <112455130+mmajewsky133@users.noreply.github.com>
2022-11-27 16:52:47 -0600

    vw_rol opcion + edicion rol_opciones

--- entidades/rol_opciones.php
+++ entidades/rol_opciones.php
@@ -3,17 +3,36 @@
 class rol_opciones
 {
     private $id_rol_opciones;
-    private $rol_id_rol;
-    private $opciones_id_opciones;
+    private $tbl_rol_id_rol;
+    private $tbl_opciones_id_opciones;
 
-
-    public function GET($k)
+    public function getIdRolUsuario()
     {
-        return $this->$k;
+        return $this->id_rol_opciones;
     }
 
-    public function SET($k, $v)
+    public function setIdRolUsuario($id_rol_opciones)
     {
-        return $this->$k = $v;
+        $this->id_rol_opciones = $id_rol_opciones;
+    }
+
+    public function getTblRolIdRol()
+    {
+        return $this->tbl_rol_id_rol;
+    }
+
+    public function setTblRolIdRol($tbl_rol_id_rol)
+    {
+        $this->tbl_rol_id_rol = $tbl_rol_id_rol;
+    }
+
+    public function getTblOpcionesIdOpciones()
+    {
+        return $this->tbl_opciones_id_opciones;
+    }
+
+    public function setTblOpcionesIdOpciones($tbl_opciones_id_opciones)
+    {
+        $this->tbl_opciones_id_opciones = $tbl_opciones_id_opciones;
     }
 }
--- entidades/rol_usuario.php
+++ entidades/rol_usuario.php
@@ -4,32 +4,38 @@ class rol_usuario
 {
 
 
-         private $id_rol_usuario;
-         private $tbl_usuario_id_usuario;
-         private $tbl_rol_id_rol;
+    private $id_rol_usuario;
+    private $tbl_usuario_id_usuario;
+    private $tbl_rol_id_rol;
 
 
-         public function getIdRolUsuario(){
-             return $this ->id_rol_usuario;
-         }
+    public function getIdRolUsuario()
+    {
+        return $this->id_rol_usuario;
+    }
 
-         public function setIdRolUsuario($id_rol_usuario){
-             $this->id_rol_usuario = $id_rol_usuario;
-         }
+    public function setIdRolUsuario($id_rol_usuario)
+    {
+        $this->id_rol_usuario = $id_rol_usuario;
+    }
 
-         public function  getTblUsuarioIdUsuario(){
-             return $this->tbl_usuario_id_usuario;
-         }
+    public function getTblUsuarioIdUsuario()
+    {
+        return $this->tbl_usuario_id_usuario;
+    }
 
-         public function setTblUsuarioIdUsuario($tbl_usuario_id_usuario){
-             $this -> tbl_usuario_id_usuario;
-         }
+    public function setTblUsuarioIdUsuario($tbl_usuario_id_usuario)
+    {
+        $this->tbl_usuario_id_usuario = $tbl_usuario_id_usuario;
+    }
 
-         public function getTblRolIdRol(){
-             return $this->tbl_rol_id_rol;
-         }
+    public function getTblRolIdRol()
+    {
+        return $this->tbl_rol_id_rol;
+    }
 
-         public  function setTblRolIdRol($tbl_rol_id_rol){
-             $this->tbl_rol_id_rol;
-         }
+    public function setTblRolIdRol($tbl_rol_id_rol)
+    {
+        $this->tbl_rol_id_rol = $tbl_rol_id_rol;
+    }
 }
